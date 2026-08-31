bd9c10f90108288c371a86549d47dacc53bc1af9
Алексей Ладорный <aleksejladornyj@gmail.com>
2026-09-01 00:50:21 +0300

    Move all screenshots to screenshots subfolder

--- dml_ddl_dcl_4/task5.sql
+++ dml_ddl_dcl_4/task5.sql
@@ -1,25 +1,31 @@
 -- Задание 5: Функции и представления
 
 -- 1. Функция CalculateAnnualBonus (10% от зарплаты)
-CREATE OR REPLACE FUNCTION CalculateAnnualBonus(emp_id INT, salary NUMERIC)
+CREATE OR REPLACE FUNCTION CalculateAnnualBonus(emp_id INT)
 RETURNS NUMERIC
 LANGUAGE plpgsql
 AS $$
+DECLARE
+    emp_salary NUMERIC;
 BEGIN
-    RETURN salary * 0.10;
+    SELECT Salary INTO emp_salary 
+    FROM Employees 
+    WHERE EmployeeID = emp_id;
+    
+    RETURN emp_salary * 0.10;
 END;
 $$;
 
 -- 2. Посчитать бонус для каждого сотрудника
 SELECT EmployeeID, FirstName, LastName, Salary, 
-       CalculateAnnualBonus(EmployeeID, Salary) AS AnnualBonus
+       CalculateAnnualBonus(EmployeeID) AS AnnualBonus
 FROM Employees;
 
 -- 3. Создать представление IT_Department_View
 CREATE OR REPLACE VIEW IT_Department_View AS
 SELECT EmployeeID, FirstName, LastName, Salary
 FROM Employees
-WHERE Department = 'IT';
+WHERE Department = 'IT' OR Department = 'Senior IT';
 
 -- 4. Выбрать данные из представления
 SELECT * FROM IT_Department_View;
