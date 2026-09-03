93b20d8346586e52fad2ce45d31d5cab94643f20
Алексей Ладорный <aleksejladornyj@gmail.com>
2026-09-03 04:47:14 +0300

    Fix function body: use salary param, ensure correct GRANT and view only IT

--- dml_ddl_dcl_4/task5.sql
+++ dml_ddl_dcl_4/task5.sql
@@ -1,31 +1,24 @@
 -- Задание 5: Функции и представления
 
 -- 1. Функция CalculateAnnualBonus (10% от зарплаты)
-CREATE OR REPLACE FUNCTION CalculateAnnualBonus(emp_id INT)
+CREATE OR REPLACE FUNCTION CalculateAnnualBonus(emp_id INT, salary NUMERIC)
 RETURNS NUMERIC
 LANGUAGE plpgsql
 AS $$
-DECLARE
-    emp_salary NUMERIC;
 BEGIN
-    SELECT Salary INTO emp_salary 
-    FROM Employees 
-    WHERE EmployeeID = emp_id;
-    
     RETURN emp_salary * 0.10;
 END;
 $$;
 
 -- 2. Посчитать бонус для каждого сотрудника
-SELECT EmployeeID, FirstName, LastName, Salary, 
-       CalculateAnnualBonus(EmployeeID) AS AnnualBonus
+SELECT EmployeeID, FirstName, Salary, CalculateAnnualBonus(EmployeeID, Salary) AS Bonus
 FROM Employees;
 
 -- 3. Создать представление IT_Department_View
 CREATE OR REPLACE VIEW IT_Department_View AS
 SELECT EmployeeID, FirstName, LastName, Salary
 FROM Employees
-WHERE Department = 'IT' OR Department = 'Senior IT';
+WHERE Department = 'IT';
 
 -- 4. Выбрать данные из представления
 SELECT * FROM IT_Department_View;
